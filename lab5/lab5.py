8ff4b3009a2990ddc86e85a75bfb9d530219aaa0
shogbom <samuel.aronson.hogbom@gmail.com>
2026-02-24 10:50:14 +0100

    onadjna

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -8,7 +8,7 @@ n=600
 python_n = 10
 description = f"Collections.sort-{inFile}-{n}"
 javaResultFile = f"{description}.csv"
-pythonResultFile = f"python_averages.csv"
+pythonResultFile = f"python_averages_{python_n}.csv"
 
 for i in range (10): 
     print(f"Starting time measurements, {description}")
@@ -17,8 +17,7 @@ for i in range (10):
     #läs ut-filen med 600 tider, räkna ut medelvärde i jämviktsläget, spara medelvärdet
     df = pd.read_csv(javaResultFile, index_col=0)
     averages = df[350:].mean() # 350 insvängning
-    averages.to_csv(pythonResultFile)
+    averages.to_csv(pythonResultFile, mode='a', header=False, index=False)
 
 # analyse the measured times
-plt.savefig(f"{javaResultFile}.png")
-plt.show()
+#plt.savefig(f"{javaResultFile}.png")
