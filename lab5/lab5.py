a101297a52658447a810f0dd41a1797a7e9a28e9
lucas-mansson <lu5132ma-s@student.lu.se>
2026-02-24 12:41:33 +0100

    aosjh

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,15 +1,32 @@
+from csv import Error
 import subprocess
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as stats
+import sys
+
+if(len(sys.argv) != 4):
+    raise Error("Usage: python lab5.py: [python_n] [algoritm] [jit]")
+
+python_n = int(sys.argv[1]) # antal körningar i python, 10 eller 100
+algoritm = sys.argv[2]
+jit = sys.argv[3]
+
+if(python_n not in [10, 100]):
+    raise Error("python_n must be 10 or 100")
+
+if(algoritm not in["collections", "own"]):
+    raise Error("algoritm must be 'collections' or 'own'")
+
+if(jit not in ["jit", "no_jit"]):
+    raise Error("jit must be 'jit' or 'no-jit'")
 
 # run the experiment
 inFile = "data1.txt"
 n=600
-python_n = 100
-java_results = f"Collections.sort-{inFile}-{n}.csv"
-python_averages = f"python_averages_{python_n}.csv"
+java_results = f"{algoritm}.sort-{inFile}-{n}.csv"
+python_averages = f"averages_{python_n}_{algoritm}_{jit}.csv"
 
 averages = []
 for i in range(python_n): 
@@ -31,6 +48,6 @@ pd.Series(np.array(averages)).to_csv(python_averages, index=False)
 data = pd.read_csv(python_averages)
 mean = data.mean()
 std_err = stats.sem(data)  # Standard error of the mean
-print(f"T test: { stats.t.interval(confidence=0.95, df=len(data)-1, loc=mean, scale=std_err)}")
+print(f"konfidensintervall: { stats.t.interval(confidence=0.95, df=len(data)-1, loc=mean, scale=std_err)}")
 print(f"mean: {mean}")
 
