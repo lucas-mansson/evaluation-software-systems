e67bb45451ef9da6282e7657c22f82cd404d89bd
lucas-mansson <lu5132ma-s@student.lu.se>
2026-02-24 10:43:36 +0100

    asfba

--- lab5/Measure.java
+++ lab5/Measure.java
@@ -1,74 +1,80 @@
 import java.io.*;
 import java.util.*;
 
-public class Measure{
-	public static void main(String args[]){
-		String inputFile = args[0];
-		String outputFile = args[1];
-		int n = Integer.parseInt(args[2]);
-		LinkedList<Integer> numbers = read(inputFile);
-		try (PrintWriter writer = new PrintWriter(new FileWriter(outputFile))) {
-			writer.println("index,time");
-			for(int i = 0; i < n; i++){
-				LinkedList<Integer> temp1 = new LinkedList<>(numbers);
-				long start = System.nanoTime();
-				sortOwn(temp1);
-				long end = System.nanoTime();
-				temp1 = new LinkedList<>(numbers);
-				long timeForNoimp = end - start;
-				//start = System.nanoTime();
-				//sortOwn(temp1);
-				//end = System.nanoTime();
-				//writer.println(i + ";" + (timeForNoimp) + ";" + (end-start) + ";");
-				writer.println(i + "," + timeForNoimp);
-				System.out.println(i);
-			}
-        	} catch (IOException e) {
-            		System.out.println("Error writing file: " + e.getMessage());
-        	}
-	}
-	public static LinkedList<Integer> read(String inputFile){
-		LinkedList<Integer> numbers = new LinkedList<>();
-		try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
-            		String line;
-            		while ((line = br.readLine()) != null) {
-                		numbers.add(Integer.parseInt(line.trim()));
-            		}
-        	} catch (IOException e) {
-            		System.out.println("Fel vid läsning av fil: " + e.getMessage());
-            		return null;
-        	}
-		return numbers; 
-	}
-	public static LinkedList<Integer> sortC(LinkedList<Integer> list){
-		Collections.sort(list);
-		return list;
-	}
-	public static LinkedList<Integer> sortOwn(LinkedList<Integer> list){
-		ListSorter.sort(list);
-		return list;
-	}
-	public class ListSorter{
+public class Measure {
+    public static void main(String args[]) {
+        String inputFile = args[0];
+        String outputFile = args[1];
+        int n = Integer.parseInt(args[2]);
 
-		public static LinkedList<Integer> sort(LinkedList<Integer> list) {
+        LinkedList<Integer> numbers = read(inputFile);
+        try (PrintWriter writer = new PrintWriter(new FileWriter(outputFile))) {
 
-    			LinkedList<Integer> sorted = new LinkedList<>();
+            writer.println("index,time");
 
-    			for (Integer value : list) {
+            for (int i = 0; i < n; i++) {
+                LinkedList<Integer> temp1 = new LinkedList<>(numbers);
+                long start = System.nanoTime();
 
-        			if (sorted.isEmpty()) {
-            				sorted.add(value);
-            				continue;
-        			}
+                sortC(temp1);
 
-        			int index = 0;
-        			while (index < sorted.size() && sorted.get(index) <= value) {
-            				index++;
-        			}
+                long end = System.nanoTime();
+                temp1 = new LinkedList<>(numbers);
 
-        			sorted.add(index, value);
-    			}
-    			return sorted;
-		}
-	}
+                long timeForNoimp = end - start;
+                writer.println(i + "," + timeForNoimp);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error writing file: " + e.getMessage());
+        }
+    }
+
+    public static LinkedList<Integer> read(String inputFile) {
+        LinkedList<Integer> numbers = new LinkedList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                numbers.add(Integer.parseInt(line.trim()));
+            }
+        } catch (IOException e) {
+            System.out.println("Fel vid läsning av fil: " + e.getMessage());
+            return null;
+        }
+        return numbers;
+    }
+
+    public static LinkedList<Integer> sortC(LinkedList<Integer> list) {
+        Collections.sort(list);
+        return list;
+    }
+
+    public static LinkedList<Integer> sortOwn(LinkedList<Integer> list) {
+        ListSorter.sort(list);
+        return list;
+    }
+
+    public class ListSorter {
+
+        public static LinkedList<Integer> sort(LinkedList<Integer> list) {
+
+            LinkedList<Integer> sorted = new LinkedList<>();
+
+            for (Integer value : list) {
+
+                if (sorted.isEmpty()) {
+                    sorted.add(value);
+                    continue;
+                }
+
+                int index = 0;
+                while (index < sorted.size() && sorted.get(index) <= value) {
+                    index++;
+                }
+
+                sorted.add(index, value);
+            }
+            return sorted;
+        }
+    }
 }
--- lab5/lab5.py
+++ lab5/lab5.py
@@ -5,14 +5,20 @@ import matplotlib.pyplot as plt
 # run the experiment
 inFile = "data1.txt"
 n=600
+python_n = 10
 description = f"Collections.sort-{inFile}-{n}"
-resultFile = f"{description}.csv"
+javaResultFile = f"{description}.csv"
+pythonResultFile = f"python_averages.csv"
 
-print(f"Starting time measurements, {description}")
-subprocess.run(["java", "Measure",  inFile, resultFile,  str(n)])
+for i in range (10): 
+    print(f"Starting time measurements, {description}")
+    subprocess.run(["java", "Measure",  inFile, javaResultFile,  str(n)])
+
+    #läs ut-filen med 600 tider, räkna ut medelvärde i jämviktsläget, spara medelvärdet
+    df = pd.read_csv(javaResultFile, index_col=0)
+    averages = df[350:].mean() # 350 insvängning
+    averages.to_csv(pythonResultFile)
 
 # analyse the measured times
-df = pd.read_csv(resultFile, index_col=0)
-df.plot()
+plt.savefig(f"{javaResultFile}.png")
 plt.show()
-print(df)
