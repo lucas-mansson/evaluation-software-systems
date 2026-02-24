b432fff1de9708ac8358f6cae17f8a5274db8a92
lucas-mansson <lu5132ma-s@student.lu.se>
2026-02-24 12:15:51 +0100

    ajisdbgahsoidhj

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,23 +1,36 @@
 import subprocess
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
 
 # run the experiment
 inFile = "data1.txt"
 n=600
-python_n = 10
-description = f"Collections.sort-{inFile}-{n}"
-javaResultFile = f"{description}.csv"
-pythonResultFile = f"python_averages_{python_n}.csv"
+python_n = 100
+java_results = f"Collections.sort-{inFile}-{n}.csv"
+python_averages = f"python_averages_{python_n}.csv"
 
-for i in range (10): 
-    print(f"Starting time measurements, {description}")
-    subprocess.run(["java", "Measure",  inFile, javaResultFile,  str(n)])
+averages = []
+for i in range(python_n): 
+    percent = int((i/(python_n-1))*100)
+    print(f"\r[{"█" * percent}{" " * (100-percent-1)}] {percent}%" , end="", flush=True)
+
+    subprocess.run(["java", "Measure",  inFile, java_results,  str(n)])
 
     #läs ut-filen med 600 tider, räkna ut medelvärde i jämviktsläget, spara medelvärdet
-    df = pd.read_csv(javaResultFile, index_col=0)
-    averages = df[350:].mean() # 350 insvängning
-    averages.to_csv(pythonResultFile, mode='a', header=False, index=False)
+    df = pd.read_csv(java_results)
+    averages.append(df["time"][350:].mean()) # 350 insvängning
 
-# analyse the measured times
-#plt.savefig(f"{javaResultFile}.png")
+print("\n")
+
+# Konvertera till numpy array sen series för att skriva till csv
+pd.Series(np.array(averages)).to_csv(python_averages, index=False)
+
+# räkna ut medelvärde och konfidensintervall för de 10/100 sparade medelvärdena
+data = pd.read_csv(python_averages)
+mean = data.mean()
+std_err = stats.sem(data)  # Standard error of the mean
+print(f"T test: { stats.t.interval(confidence=0.95, df=len(data)-1, loc=mean, scale=std_err)}")
+print(f"mean: {mean}")
+
