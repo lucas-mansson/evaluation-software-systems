5a48b4ca773a3323b21ec2872450e4684951bc09
lucas-mansson <lu5132ma-s@student.lu.se>
2026-03-02 17:28:13 +0100

    .

--- lab5/analysis.py
+++ lab5/analysis.py
@@ -17,40 +17,52 @@ own_path_no_jit_data2 = "data/own/*_data2.csv"
 
 # coll jit
 file_list = glob.glob(coll_path_jit)
-coll_dfs_jit = [pd.read_csv(file) for file in file_list]
-coll_jit = pd.concat(coll_dfs_jit, ignore_index=True)
-
-print(coll_path_jit)
-print(coll_jit)
+dfs = [pd.read_csv(file) for file in file_list]
+coll_jit = pd.concat(dfs, ignore_index=True)
 
 #coll jit data 2
+file_list = glob.glob(coll_path_jit_data2)
+dfs = [pd.read_csv(file) for file in file_list]
+coll_jit_data2 = pd.concat(dfs, ignore_index=True)
 
 # coll not jit
 file_list = glob.glob(coll_path_no_jit)
-coll_dfs_no_jit = [pd.read_csv(file) for file in file_list]
-coll_no_jit = pd.concat(coll_dfs_no_jit, ignore_index=True)
+dfs = [pd.read_csv(file) for file in file_list]
+coll_no_jit = pd.concat(dfs, ignore_index=True)
 
-print(coll_path_no_jit)
-print(coll_no_jit)
+# coll not jit data2
+file_list = glob.glob(coll_path_no_jit_data2)
+dfs = [pd.read_csv(file) for file in file_list]
+coll_no_jit_data2 = pd.concat(dfs, ignore_index=True)
 
 # Own jit
 file_list = glob.glob(own_path_jit)
-own_jits = [pd.read_csv(file) for file in file_list]
-own_jit = pd.concat(own_jits, ignore_index=True)
+pds = [pd.read_csv(file) for file in file_list]
+own_jit = pd.concat(pds, ignore_index=True)
 
-print(own_path_jit)
-print(own_jit)
+#own jit data2
+file_list = glob.glob(own_path_jit_data2)
+pds = [pd.read_csv(file) for file in file_list]
+own_jit_data2 = pd.concat(pds, ignore_index=True)
 
 # own no jit
 file_list = glob.glob(own_path_no_jit)
-own_no_jits = [pd.read_csv(file) for file in file_list]
-own_no_jit = pd.concat(own_no_jits, ignore_index=True)
+pds = [pd.read_csv(file) for file in file_list]
+own_no_jit = pd.concat(pds, ignore_index=True)
 
-print(own_path_no_jit)
-print(own_no_jit)
+#own no jit data2
+file_list = glob.glob(own_path_no_jit_data2)
+pds = [pd.read_csv(file) for file in file_list]
+own_no_jit_data2 = pd.concat(pds, ignore_index=True)
+
 
 t_jit, p_val_jit = st.ttest_ind(coll_jit["0"], own_jit["0"])
+t_jit_data2, p_val_jit_data2 = st.ttest_ind(coll_jit_data2["0"], own_jit_data2["0"])
+
 t_no_jit, p_val_no_jit = st.ttest_ind(coll_no_jit["0"], own_no_jit["0"])
+t_no_jit_data2, p_val_no_jit_data2 = st.ttest_ind(coll_no_jit_data2["0"], own_no_jit_data2["0"])
 
 print(f"T test jit: {t_jit}, {p_val_jit}")
+print(f"T test jit data2: {t_jit_data2}, {p_val_jit_data2}")
 print(f"T test no jit: {t_no_jit}, {p_val_no_jit}")
+print(f"T test no jit data2: {t_no_jit_data2}, {p_val_no_jit_data2}")
